0f12ee0bd94188748e297185fad1b4369990a3c1
Philipp <pd95@users.noreply.github.com>
2020-12-21 22:29:30 +0100

    Fixing Tile definition to avoid horizontal scrolling in `StickyHeader`

--- GeometryReader/Part-3/Tile.swift
+++ GeometryReader/Part-3/Tile.swift
@@ -10,22 +10,22 @@ import SwiftUI
 struct Tile: View {
     var imageName = ""
     var tileLabel = ""
-
+    
     var body: some View {
-        Image(imageName)
-            .resizable()
-            .aspectRatio(contentMode: .fill)
+        Text(tileLabel)
+            .padding(.bottom, 20)
+            .opacity(0.85)
+            .font(.system(size: 30, weight: .black))
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
             .frame(height: 200, alignment: .bottom)
-            .cornerRadius(20)
+            .background(
+                Image(imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 20))
             .shadow(color: .gray, radius: 10, x: 0, y: 5)
-            .overlay(VStack {
-                Spacer()
-                Text(tileLabel)
-                    .padding(.bottom, 20)
-                    .opacity(0.85)
-                    .font(.system(size: 30, weight: .black))
-                    .foregroundColor(.white)
-            })
     }
 }
  
